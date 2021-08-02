fdb8b7df0006ca36ae3a21fd89b3cd040fc430ee
Matthaus Schall Vizuete <m.schall.vizuete@gmail.com>
2021-08-02 16:24:44 +0200

    Removendo duplicacao de codigo para configurar adapter x recyclerview

--- Playground06/app/src/main/java/br/com/matthaus/playground06/MainActivity.kt
+++ Playground06/app/src/main/java/br/com/matthaus/playground06/MainActivity.kt
@@ -11,19 +11,35 @@ class MainActivity : AppCompatActivity() {
 
     val turmaUm = Turma("Android", "15:00", 5)
 
+    var recyclerViewListaAlunos: RecyclerView? = null
+    var adapterListaAlunos: TurmaAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val buttonMatricular = findViewById<Button>(R.id.button_matricular)
-        val recyclerViewListaAlunos = findViewById<RecyclerView>(R.id.rv_lista_alunos)
+        recyclerViewListaAlunos = findViewById<RecyclerView>(R.id.rv_lista_alunos)
 
         buttonMatricular.setOnClickListener {
-            executarMatricula(recyclerViewListaAlunos)
+            executarMatricula()
         }
     }
 
-    fun executarMatricula(recyclerViewListaAlunos: RecyclerView) {
+    fun configurarAdapter(listaAlunos: List<Pair<String, String>>) {
+        //Criar um adaptador para mostrar a lista de alunos matriculados
+        adapterListaAlunos = TurmaAdapter(listaAlunos) { nome, documento, posicao ->
+            executarDesmatricular(nome, documento, posicao)
+        }
+        recyclerViewListaAlunos?.let {
+            //Definir que tipo de disposição de itens vai ser utilizado
+            it.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+            //Passar para o recyclerview qual o adaptador vai ser utilizado, ou seja, o criado anteriormente (instanciado)
+            it.adapter = adapterListaAlunos
+        }
+    }
+
+    fun executarMatricula() {
         //Buscar o nome e o documento digitado
         val nome = "Matthaus"
         val documento = "MG123456"
@@ -32,32 +48,18 @@ class MainActivity : AppCompatActivity() {
 
         if (matriculadoSucesso) {
             Toast.makeText(this, "Aluno matriculado com sucesso", Toast.LENGTH_SHORT).show()
-
-            //Criar um adaptador para mostrar a lista de alunos matriculados
-            val turmaAdapter = TurmaAdapter(turmaUm.getAlunosMatriculados()) { nome, documento, posicao ->
-                executarDesmatricular(nome, documento, posicao, recyclerViewListaAlunos)
-            }
-            //Definir que tipo de disposição de itens vai ser utilizado
-            recyclerViewListaAlunos.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-            //Passar para o recyclerview qual o adaptador vai ser utilizado, ou seja, o criado anteriormente (instanciado)
-            recyclerViewListaAlunos.adapter = turmaAdapter
+            configurarAdapter(turmaUm.getAlunosMatriculados())
         } else {
             Toast.makeText(this, "Erro ao matricular aluno", Toast.LENGTH_SHORT).show()
         }
 
     }
 
-    fun executarDesmatricular(nome: String, documento: String, posicao: Int, recyclerViewListaAlunos: RecyclerView) {
+    fun executarDesmatricular(nome: String, documento: String, posicao: Int) {
         val sucesso = turmaUm.desmatricularAluno(posicao)
         if (sucesso) {
             Toast.makeText(this, "Desmatricula com sucesso: " + nome + " / " + documento + " / " + posicao.toString(), Toast.LENGTH_SHORT).show()
-
-            val turmaAdapter = TurmaAdapter(turmaUm.getAlunosMatriculados()) { nome, documento, posicao ->
-                executarDesmatricular(nome, documento, posicao, recyclerViewListaAlunos)
-            }
-
-            recyclerViewListaAlunos.adapter = turmaAdapter
-
+            configurarAdapter(turmaUm.getAlunosMatriculados())
         } else {
             Toast.makeText(this, "Desmatricula não realizada", Toast.LENGTH_SHORT).show()
         }
